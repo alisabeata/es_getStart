f9745309f9b41be75075ae748b74ad51b8872e93
alisabeata <alisa314159@gmail.com>
2018-12-26 18:22:02 +0200

    generators

--- manual/32_generators.js
+++ manual/32_generators.js
@@ -11,6 +11,7 @@ function* generator() {
 
 // тк function* возвр итератор, его можно перебирать циклом for of
 
+// yield прерывает выполнения генератора, при вызове next продолжает с текущего yield
 
 function* generator() {
   yield 1;
@@ -23,6 +24,7 @@ function* generator() {
 const interator = generator();
 
 interator.next(2) // в const a вернётся значение 2
+
 
 
 
@@ -42,3 +44,27 @@ let b = gen.next().value
 console.log('b', b)    // >>> 'b', 1
 
 gen.next(2).value
+
+
+
+// для передачи генератора в генератор как параметра исп yield*
+
+function* anotherGenerator(i) {
+  yield i + 1;
+  yield i + 2;
+  yield i + 3;
+}
+
+function* generator(i) {
+  yield i;
+  yield* anotherGenerator(i);
+  yield i + 10;
+}
+
+let gen = generator(10);
+
+console.log(gen.next().value); // 10
+console.log(gen.next().value); // 11
+console.log(gen.next().value); // 12
+console.log(gen.next().value); // 13
+console.log(gen.next().value); // 20
